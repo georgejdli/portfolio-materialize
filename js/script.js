72662f2a19e23cc61956b8c0f81afc796a67ab52
George Li <georgejdli@gmail.com>
2015-02-17 21:52:47 -0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+});
+
+//only required if images are added dynamically
+/*$(document).ready(function(){               
+    $('.materialboxed').materialbox();
+});*/
